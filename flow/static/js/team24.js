152bce8a4deca083379e3f5b6aeecdda1eaf39fe
Prasanthisandaka2005 <prasanthisandaka2005@gmail.com>
2024-04-06 20:05:28 +0530

    Teams Page Updated

--- flow/static/js/team24.js
+++ flow/static/js/team24.js
@@ -1,41 +1,47 @@
 /* START CODE FOR NAVBAR */
-const hamburgerMenu = document.querySelector("#hamburger-menu");
-const overlay = document.querySelector("#overlay");
-const nav1 = document.querySelector("#nav-1");
-const nav2 = document.querySelector("#nav-2");
-const nav3 = document.querySelector("#nav-3");
-const nav4 = document.querySelector("#nav-4");
-const nav5 = document.querySelector("#nav-5");
-const navItems = [nav1, nav2, nav3, nav4, nav5];
+// const hamburgerMenu = document.querySelector("#hamburger-menu");
+// const overlay = document.querySelector("#overlay");
+// const nav1 = document.querySelector("#nav-1");
+// const nav2 = document.querySelector("#nav-2");
+// const nav3 = document.querySelector("#nav-3");
+// const nav4 = document.querySelector("#nav-4");
+// const nav5 = document.querySelector("#nav-5");
+// const navItems = [nav1, nav2, nav3, nav4, nav5];
 
-function navAnimation(val1, val2) {
-    navItems.forEach((nav, i) => {
-        nav.classList.replace(`slide-${val1}-${i + 1}`, `slide-${val2}-${i + 1}`);
-    });
-}
+// function navAnimation(val1, val2) {
+//     navItems.forEach((nav, i) => {
+//         nav.classList.replace(`slide-${val1}-${i + 1}`, `slide-${val2}-${i + 1}`);
+//     });
+// }
 
-function toggleNav() {
-    hamburgerMenu.classList.toggle("active");
+// function toggleNav() {
+//     hamburgerMenu.classList.toggle("active");
 
-    overlay.classList.toggle("overlay-active");
+//     overlay.classList.toggle("overlay-active");
 
-    if (overlay.classList.contains("overlay-active")) {
-        overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+//     if (overlay.classList.contains("overlay-active")) {
+//         overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
 
-        navAnimation("out", "in");
-    } else {
-        overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
+//         navAnimation("out", "in");
+//     } else {
+//         overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
 
-        navAnimation("in", "out");
-    }
-}
+//         navAnimation("in", "out");
+//     }
+// }
 
-hamburgerMenu.addEventListener("click", toggleNav);
-navItems.forEach((nav) => {
-    nav.addEventListener("click", toggleNav);
-});
+// hamburgerMenu.addEventListener("click", toggleNav);
+// navItems.forEach((nav) => {
+//     nav.addEventListener("click", toggleNav);
+// });
 /* CODE FOR NAVBAR END  */
 
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const container = document.querySelector(".container");
+
+hamburger_menu.addEventListener("click", () => {
+    container.classList.toggle("active");
+});
 
 /* START CODE FOR TEAM */
 
@@ -60,11 +66,11 @@ navItems.forEach((nav) => {
 
 
 /* CODE FOR CARD */
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const cards = document.querySelectorAll('.card');
 
     cards.forEach(card => {
-        card.addEventListener('click', function() {
+        card.addEventListener('click', function () {
             const hamburgerMenu = document.querySelector("#hamburger-menu");
             hamburgerMenu.classList.add('hide-transition');
             setTimeout(() => {
@@ -81,7 +87,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const cards = document.querySelectorAll('.card');
 
     cards.forEach(card => {
